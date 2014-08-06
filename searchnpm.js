03be6c87f1f32dcab4904dda43fb8cb76e3be164
faiq <faiqrazarizvi@gmail.com>
2014-08-05 19:40:49 -0700

    added a retry mechanisim, fixed queries a bit

--- searchnpm.js
+++ searchnpm.js
@@ -13,23 +13,31 @@ function Searchnpm (esUrl){
   this.client = new elasticsearch.Client({ 
     host: _this.esUrl
   })
+  this.ready = false
+  var _this = this 
   this.client.ping({ 
     requestTimeout: 1000, 
     hello: 'elasticsearch!'
   }, function (error) { 
     if (error) throw Error('Elasticsearch is having trouble connecting the url you gave it') 
-    else console.log('connected to ES')   
+    else{ 
+      console.log('connected to ES')
+      _this.ready = true
+    } 
   })
   this.allKeys = ['searchBy' , 'searchQuery', 'sortBy', 'page', 'sortOrd', 'from', 'size']
   this.mandKeys = ['searchQuery']  
   this.validSearchBy = ['keyword', 'description', 'packagename', 'author', 'maintainers', 'dependencies', 'general']
   this.vaildSortBy = ['relevance', 'issues', 'stars', 'githubstars', 'downloads', 'date']
+  this.retryCounter = 0
 }
 
 Searchnpm.prototype.searchPackages = function (searchObj, callback){ 
+  var _this = this
   searchObj = this.validateJson(searchObj)
   var query = this.buildQueries(searchObj)
-  if (query){  
+  console.log(this.ready)
+  if (this.ready){  
     this.client.search(query, function (err, results){ 
       if (err){ 
         callback(err, null)
@@ -38,42 +46,49 @@ Searchnpm.prototype.searchPackages = function (searchObj, callback){
         callback(null, results)
       } 
     })
+  }else{ 
+    if (this.retryCounter == 5){
+      callback({err: 'search retried too many times'}, null) 
+      process.exit(0)  
+    }  
+    setTimeout(function(){ 
+      _this.searchPackages(searchObj, callback) 
+      _this.retryCounter++
+    }, 1500)
   }
 }
-
 Searchnpm.prototype.validateJson = function (searchObj){
-  if (typeof searchObj === 'object'){
-    Object.keys(this.mandKeys).forEach(function (key) {
-      if(!searchObj.hasOwnProperty(key)) 
-        throw Error('You passed in an invalid JSON obj\n, it should look like{search-querey: <argument>}')
-    })
-    //fill in minimum parameters  
-    searchObj.searchBy = searchObj.searchBy || 'general'
-    searchObj.sortBy = searchObj.sortBy || 'relevance'
-    searchObj.from = searchObj.from || 0
-    searchObj.size = searchObj.size || 1000   //big number probably will end up changing in future   
-    //check to see if their shit is okay 
-    
-    if (this.valid_searchBy.indexOf(searchObj.searchBy) === -1) // its not there
-      throw Error('The searchBy field must be one of the follwing' + this.validSearchBy) 
-    
-    if (this.valid_sortBy.indexOf(searchObj.sortBy) === -1)
-      throw Error('The sortBy field must be one of the following' + this.validSortBy)  
- 
-    if (searchObj.sortOrd && (searchObj.sortOrd != 'asc' || searchObj.sortOrd != 'desc'))
-      throw Error('You can only sort by ascending or descending order, respectively corresponding to asc and desc')
-
-    return searchObj  
-  }else if(typeof searchObj === 'string'){ 
+  if(typeof searchObj === 'string'){ 
     //make a default JSON object to search by most likely going to be used by the cli 
     //this will make a default obj with from being 0 and size 
     var tempObj = {} 
     tempObj.searchBy = 'general'
     tempObj.sortBy = 'relevance'
     tempObj.from =  0
-    tempObj.size = 1000   //big number probably will end up changing in future   
-       
+    tempObj.searchQuery = searchObj
+    tempObj.size = 100   //big number probably will end up changing in future   
     return tempObj
+  }else if (typeof searchObj === 'object'){
+    this.mandKeys.forEach(function (key) {
+      if(!searchObj.hasOwnProperty(key)) 
+        throw Error('You passed in an invalid JSON obj, it should look like{searchQuery: <argument>}')
+    })
+    //fill in minimum parameters  
+    searchObj.searchBy = searchObj.searchBy || 'general'
+    searchObj.sortBy = searchObj.sortBy || 'relevance'
+    searchObj.from = searchObj.from || 0
+    searchObj.size = searchObj.size || 100   //big number probably will end up changing in future   
+   
+    if ((this.validSearchBy).indexOf(searchObj.searchBy) === -1) // its not there
+      throw Error('The searchBy field must be one of the follwing' + this.validSearchBy) 
+    
+    if ((this.vaildSortBy).indexOf(searchObj.sortBy) === -1)
+      throw Error('The sortBy field must be one of the following' + this.validSortBy)  
+ 
+    if (searchObj.sortOrd && (searchObj.sortOrd != 'asc' || searchObj.sortOrd != 'desc'))
+      throw Error('You can only sort by ascending or descending order, respectively corresponding to asc and desc')
+
+    return searchObj  
   }else
     throw Error('You must either pass in a string or an object to make a query') 
 } 
@@ -90,7 +105,7 @@ Searchnpm.prototype.buildQueries = function (searchObj){
       return QueryFactory.searchDescription(searchObj) 
       break  
     case 'packagename': 
-      return QueryFactory.searchName(searchObj)  
+      return QueryFactory.searchPackageName(searchObj)  
       break 
     case 'author': 
       return QueryFactory.searchAuthor(searchObj) 
